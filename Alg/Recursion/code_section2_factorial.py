bda11c007a43627c6b507c173ddca105bb7d5958
epengc <epengc@icloud.com>
2021-06-22 14:52:56 -0400

    update folder system

---
+++ Alg/Recursion/code_section2_factorial.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+import sys
+sys.setrecursionlimit(10000)
+
+
+def factorial(n):
+    assert n>=0 and int(n) == n, 'The number must be positive integer only!'
+    if n in [0,1]:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+def main():
+
+    print(factorial(10))
+
+    return 0;
+
+if __name__ == "__main__":
+
+    main()
